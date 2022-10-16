71556b1ff05ebdb60472fcf426ff31fdd796e46d
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-10-16 22:32:50 +0400

    new spreint design fxd

--- components/interface/Slider.vue
+++ components/interface/Slider.vue
@@ -1,14 +1,16 @@
 <template>
 <swiper class="swiper-container">
   <div class="swiper-wrapper">
-    <div class="swiper-slide">
-      <img class="h-64 w-64" src="~assets/img/home/main-left.jpg" />
-    </div>
-    <div class="swiper-slide">
-      <img class="h-64 w-64" src="~assets/img/home/main-right.jpg" />
-    </div>
-    <div class="swiper-slide">
-      <img class="h-64 w-64" src="~assets/img/home/main-left.jpg" />
+    <div class="swiper-slide slide">
+      <div class="slide__wrapper">
+        <img class="slide__img" src="~assets/img/home/carusel/slide01.png" />
+        <div class="slide__content">
+          <h2 class="slide__title">
+            <b class="slide__orange">Ковровые изделия</b> для <span class="nowrap">Вашего дома <span class="slide__point">.</span></span>
+          </h2>
+          <button class="slide__button">Открыть каталог</button>
+        </div>
+      </div>
     </div>
   </div>
 </swiper>
@@ -22,14 +24,69 @@ export default {
     new Swiper('.swiper-container', {
       autoplay: true,
       loop: true,
-      slidesPerView: 2,
+      slidesPerView: 1,
     })
   }
 }
 </script>
 
-<style scoped lang="scss">
-  img {
-    max-width: 100%;
+<style lang="scss">
+@import '~assets/scss/partials/mixins';
+@import '~assets/scss/partials/var-colors';
+
+.slide {
+  &__img {
+    @include img(cover);
+    max-width: 750px;
   }
+
+  &__wrapper {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__content {
+    max-width: 600px;
+  }
+
+  &__title {
+    font-family: 'Federo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 80px;
+    line-height: 90px;
+    letter-spacing: 0.02em;
+    color: $blackColor060;
+    margin-bottom: 40px;
+  }
+
+  &__button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    background: #3A3A3A;
+    ;
+    padding: 8px 16px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: -0.035em;
+    text-align: center;
+    color: $whiteColor;
+  }
+
+  &__point {
+    font-size: 90px;
+    display: inline-block;
+    border-radius: 50%;
+    color: $redColor;
+  }
+
+  &__orange {
+    color: $redColor;
+    font-weight: 800;
+  }
+}
 </style>
--- components/interface/home/SectionMain.vue
+++ components/interface/home/SectionMain.vue
@@ -2,12 +2,9 @@
 <section class="main">
   <div class="main__wrapper">
     <div class="container">
-      <h1 class="main__title">
-        <b class="main__orange">Ковровые Изделия</b> Вашей мечты<span class="main__point">.</span>
+      <h1 class="visually-hidden">
+        Магазин Ковров, с большим выбором дорожек и ковролинов, прикроватных ковриков и ковровых издейлий на заказ.
       </h1>
-      <p class="main__description">
-        Новая коллекция ковров от Merinos
-      </p>
       <Slider />
     </div>
   </div>
@@ -15,8 +12,6 @@
 </template>
 
 <style lang="scss" scoped>
-@import '~assets/scss/partials/var-colors';
-
 .main {
   &__wrapper {
     background-color: #F6F6F6;
@@ -24,35 +19,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    min-height: 750px;
-  }
-
-  &__title {
-    font-family: 'Federo';
-    font-weight: 500;
-    font-size: 60px;
-    line-height: 120%;
-    text-align: center;
-    color: $blackColor060;
-  }
-
-  &__description {
-    font-family: 'Federo';
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 140%;
-    text-align: center;
-    color: $greyColor888;
-  }
-
-  &__point {
-    font-size: 80px;
-    color: $redColor;
-  }
-
-  &__orange {
-    color: $redColor;
-    font-weight: 800;
+    min-height: 740px;
   }
 }
 </style>
